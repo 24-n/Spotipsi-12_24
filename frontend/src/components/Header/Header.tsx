576098b92746ecca06d26e1189fcc750322ad4d2
24-n <77553311no@gmail.com>
2026-03-18 10:34:06 +0200

    header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
 import useStyles from "./HeaderStyles";
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
 
 function Header() {
-
-  const {classes} = useStyles();
-  return(
-      <div className={classes.Header}>header</div>
+  const { classes } = useStyles();
+  return (
+    <div className={classes.Header}>
+      <h3 className={classes.title}>SpotiPSI<AudiotrackIcon /></h3>
+    </div>
   );
 }
 export default Header
--- frontend/src/components/Header/HeaderStyles.ts
+++ frontend/src/components/Header/HeaderStyles.ts
@@ -1,10 +1,17 @@
 import { makeStyles } from 'tss-react/mui'
 
 const useStyles = makeStyles()(() => ({
-    Header:{
-     backgroundColor:"green",
-     height:"100%"
-     
+    Header: {
+        height: "100%",
+        backgroundColor: "#313131",
+        textAlign: "end",
+        alignContent: "center"
+    },
+    title: {
+        color: "#B764DA",
+        fontFamily: "Arial, Helvetica, sans-serif",
+        margin: "0",
+        padding: "0 12px 0 0"
     }
 }));
 
