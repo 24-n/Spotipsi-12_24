de764f033869679134d3766f8f1f85f7eb154629
DaniDevML <daniellrd3@gmail.com>
2026-03-18 08:20:06 +0200

    project minimal skeleton

---
+++ frontend/src/App/App.tsx
@@ -0,0 +1,10 @@
+import useStyles from "./AppStyles";
+
+function App() {
+  
+  const {classes} = useStyles();
+  return(
+      <div className={classes.App}></div>
+  );
+}
+export default App
---
+++ frontend/src/App/AppStyles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from 'tss-react/mui'
+
+const useStyles = makeStyles()(() => ({
+    App:{
+        minWidth:"100vw",
+        minHeight:"100vh",
+       
+    }
+}));
+
+export default useStyles
